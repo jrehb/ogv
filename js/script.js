e3f5064bfa4df1af30efa24514046194ce599ce7
Juilus Rehbein <julius.rehbein@tum.de>
2026-03-06 17:34:59 +0100

    init Erste Implementierung der Website, einige Seiten noch unvollständig

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("hamburger");
+const menu = document.getElementById("nav-menu");
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  menu.classList.toggle("open");
+});
+// close menu when a link is clicked
+menu.querySelectorAll("a").forEach((a) => {
+  a.addEventListener("click", () => {
+    btn.classList.remove("open");
+    menu.classList.remove("open");
+  });
+});
