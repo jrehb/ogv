018f0fd61d91b67e00a2af69029204dbdf0db966
Juilus Rehbein <julius.rehbein@tum.de>
2026-03-08 15:20:12 +0100

    Refactor alter Code rausgeschmissen

--- js/script.js
+++ js/script.js
@@ -1,28 +1,16 @@
-// load navbar
-async function loadComponent(url, containerId) {
-  const response = await fetch(url);
-  let data = await response.text();
-  // Live-Server Injection entfernen (Kommentar + Script-Tag)
-  data = data.replace(/<!--.*?-->/gs, "");
-  data = data.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "");
-  const container = document.getElementById(containerId);
-  container.innerHTML = data;
-}
+// Drop Down Menu
+const btn = document.getElementById("hamburger");
+const menu = document.getElementById("nav-menu");
 
-loadComponent("/components/nav.html", "nav-container").then(() => {
-  const btn = document.getElementById("hamburger");
-  const menu = document.getElementById("nav-menu");
-  btn.addEventListener("click", () => {
-    btn.classList.toggle("open");
-    menu.classList.toggle("open");
-  });
-  menu.querySelectorAll("a").forEach((a) => {
-    a.addEventListener("click", () => {
-      btn.classList.remove("open");
-      menu.classList.remove("open");
-    });
-  });
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  menu.classList.toggle("open");
 });
 
-loadComponent("/components/social-media.html", "social-media-container");
-loadComponent("/components/footer.html", "footer-container");
+// close menu when a link is clicked
+menu.querySelectorAll("a").forEach((a) => {
+  a.addEventListener("click", () => {
+    btn.classList.remove("open");
+    menu.classList.remove("open");
+  });
+});
