cc4e7eee57826273206cbdc4781833b6e02cf8a4
Juilus Rehbein <julius.rehbein@tum.de>
2026-09-20 21:14:07 +0200

    feat(config): empty mail_config file

---
+++ src/config/mail_config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host'     => '',
+    'port'     => ,
+    'username' => '',
+    'password' => '',
+    'from'     => '',
+    'to'       => '',
+    'altcha_key' => '' // Zufälligen langen String einsetzen!
+];
